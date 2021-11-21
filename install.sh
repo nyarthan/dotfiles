d5b3f98a641cacd98163e20b0b3217482ee41e0d
Jannis Morgenstern <49486580+Jannis-Morgenstern@users.noreply.github.com>
2021-11-21 22:10:56 +0100

    feat: stuff

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# git
+ln -sf $HOME/Dotfiles/git/gitconfig $HOME/.gitconfig
+
+# zsh
+ln -sf $HOME/Dotfiles/zsh/zshrc $HOME/.zshrc
+
+# xorg
+ln -sf $HOME/Dotfiles/xorg/xinitrc $HOME/.xinitrc
+ln -sf $HOME/Dotfiles/xorg/Xresources $HOME/.Xresources
+sudo ln -sf $HOME/Dotfiles/xorg/xorg.conf.d/* /etc/X11/xorg.conf.d
+
+# bspwm
+ln -sf $HOME/Dotfiles/bspwm/bspwmrc $HOME/.config/bspwm/bspwmrc
+
+# sxhkd
+ln -sf $HOME/Dotfiles/sxhkd/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
+
+# picom
+ln -sf $HOME/Dotfiles/picom/picom.conf $HOME/.config/picom/picom.conf
+
+# polybar
+ln -sf $HOME/Dotfiles/polybar/config $HOME/.config/polybar/config
+ln -sf $HOME/Dotfiles/polybar/launch.sh $HOME/.config/polybar/launch.sh
+
+# alacritty
+ln -sf $HOME/Dotfiles/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+
+# nvim
+ln -sf $HOME/Dotfiles/nvim/* $HOME/.config/nvim
---
+++ nvim/vim-plug/plugins.vim
@@ -0,0 +1,10 @@
+call plug#begin('~/.nvim/plugged')
+
+Plug 'jiangmiao/auto-pairs'
+Plug 'preservim/nerdtree'
+Plug 'joshdick/onedark.vim'
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+
+call plug#end()
